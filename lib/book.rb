267636f0d6e2a22ff5b1d3e4a081d5d70cde6255
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 15:23:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
-attr_accessor :title, :author, :pages, :genre
+attr_accessor :title, :author, :page_count, :genre
 
   def initialize(title)
     @title = title
   end
-
+  
+  def turn_page
+  end
 
 end
